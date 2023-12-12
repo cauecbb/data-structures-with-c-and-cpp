b36e1582accf6aa4bfb186d3606b1d4e68a854c8
cauecbb <contato.cauecbb@gmail.com>
2023-12-12 12:58:28 -0500

    fix: added to circumference program a area calculator

--- 01-ArraysHashing/01-BasicOfC/00-CircleCircumferenceProgram.c
+++ 01-ArraysHashing/01-BasicOfC/00-CircleCircumferenceProgram.c
@@ -6,13 +6,16 @@ int main()
     const double PI = 3.14159;
     double radius;
     double circumference;
+    double area;
 
     printf("\nEnter the radius of a circle: ");
     scanf("%lf", &radius);
 
     circumference = 2 * PI * radius;
+    area = PI * radius * radius;
 
-    printf("Circumference: %lf", circumference);
+    printf("\nCircumference: %lf", circumference);
+    printf("\nArea: %lf", area);
 
     return 0;
 }
