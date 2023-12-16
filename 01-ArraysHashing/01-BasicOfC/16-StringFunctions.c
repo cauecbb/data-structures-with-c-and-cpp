d363748fc98fe9c04fcc48c0ff52bb5e8a2b477b
cauecbb <contato.cauecbb@gmail.com>
2023-12-16 14:45:32 -0500

    feature: Updated String Functions in C

--- 01-ArraysHashing/01-BasicOfC/16-StringFunctions.c
+++ 01-ArraysHashing/01-BasicOfC/16-StringFunctions.c
@@ -14,6 +14,10 @@ int main()
     // strcpy(string1, string2);          // copy string2 to string1
     // strncpy(string1, string2, 4);      // copy n characters of string2 to string1
 
+    // strset(string1, '?');              // sets all character of a string to a given character
+    // strnset(string1, 'x', 1);          // sets first n characters of a string to a given character
+    // strrev(string1);                    // reverses a string
+
     printf("\n%s", string1);
 
     return 0;
