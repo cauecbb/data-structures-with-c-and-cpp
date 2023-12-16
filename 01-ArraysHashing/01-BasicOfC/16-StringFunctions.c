ebc0b431f95922d7f8da33ba4d570cc738d11ead
cauecbb <contato.cauecbb@gmail.com>
2023-12-16 14:39:38 -0500

    feature: Added String Functions in C

---
+++ 01-ArraysHashing/01-BasicOfC/16-StringFunctions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    char string1[] = "Caue";
+    char string2[] = "CBB";
+
+    // strlwr(string1);                   // converts a string to lowercase
+    // strupr(string1);                   // converts a string to uppercase
+    // strcat(string1, string2);          // appends string 2 to end of string1
+    // strncat(string1, string2, 1);      // appends n characters from string2 to string1
+    // strcpy(string1, string2);          // copy string2 to string1
+    // strncpy(string1, string2, 4);      // copy n characters of string2 to string1
+
+    printf("\n%s", string1);
+
+    return 0;
+}
