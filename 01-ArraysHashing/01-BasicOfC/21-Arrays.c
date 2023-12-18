743f795ca9f77ac512a1a90e6947de54ff2267d9
cauecbb <contato.cauecbb@gmail.com>
2023-12-18 16:48:43 -0500

    feature: Updated Arrays in C

--- 01-ArraysHashing/01-BasicOfC/21-Arrays.c
+++ 01-ArraysHashing/01-BasicOfC/21-Arrays.c
@@ -3,9 +3,15 @@
 int main()
 {
 
-    double prices[] = {5.0, 10.0, 15.0, 20.0, 25.0};
+    double prices[] = {5.0, 10.0, 15.0, 20.0, 25.0, 30.0};
 
-    printf("\n$%.2lf", prices[0]);
+    printf("\n$%.2lf\n", prices[0]);
+
+    // array with loops
+
+    for(int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++){
+        printf("\n$%.2lf", prices[i]);
+    }
 
     return 0;
 }
